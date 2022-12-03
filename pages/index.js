005bd7386e43d7b5e2d9fb51dcff2cf72621a51a
morozovxc <xbolotovnikita@gmail.com>
2022-12-03 12:43:23 +0500

    1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { TextField, Button, Stack } from '@mui/material';
 import { useRouter } from 'next/router'
+import Head from "next/head"
 
 export default function setJudge() {
     const router = useRouter();
@@ -9,18 +10,23 @@ export default function setJudge() {
     const handleChangeName = (event) => setJudge(event.target.value)
     const handleChangeNumber = (event) => setNumber(event.target.value)
     return (
-        <Stack spacing={2}
-            left={0} right={0} top={"40%"} m={"auto"}
-            maxWidth={300}
-            position="absolute"
-        >
-            <TextField label="Фамилия" variant="filled" onChange={handleChangeName} />
-            <TextField label="Номер боя" variant="filled" onChange={handleChangeNumber} />
-            <Button
-                sx={{borderRadius: 30, background: "grey"}}
-                variant="contained"
-                onClick={() => router.push({pathname:"/judge/battle", query: { name: judge, id: number }})}
-            >Далее</Button>
-        </Stack>
+        <>
+            <Head>
+                <link rel="manifest" href="../styles/manifest.json"></link>
+            </Head>
+            <Stack spacing={2}
+                left={0} right={0} top={"40%"} m={"auto"}
+                maxWidth={300}
+                position="absolute"
+            >
+                <TextField label="Фамилия" variant="filled" onChange={handleChangeName} />
+                <TextField label="Номер боя" variant="filled" onChange={handleChangeNumber} />
+                <Button
+                    sx={{borderRadius: 30, background: "grey"}}
+                    variant="contained"
+                    onClick={() => router.push({pathname:"/judge/battle", query: { name: judge, id: number }})}
+                >Далее</Button>
+            </Stack>
+        </>
     )
 }
